b2675ef60b7fe41d123a1d4d8537bc857fbb5e16
Maicon Brendon <112555118+maiconbre@users.noreply.github.com>
2022-10-18 12:39:04 -0300

    Update utilitarios-js.js

--- scripts/js/utilitarios-js.js
+++ scripts/js/utilitarios-js.js
@@ -7,26 +7,9 @@ const email = document.getElementById('email')
 const senha = document.getElementById('senha')
 const picLogo = document.querySelector('#pic-logo')
 
-home.addEventListener("click", ()=>{
-    location.assign("../index.html");
-})
 
-servico.addEventListener("click", () => {
-    location.assign("./servicos.html");
-})
-
-contato.addEventListener("click", () => {
-    location.assign("./contatos.html");
-})
-
-login.addEventListener("click", () => {
-    location.assign("./login.html");
-})
-
-cadastro.addEventListener("click", () => {
-    location.assign("./cadastro.html");
-})
 
 picLogo.addEventListener('click',()=>{
     location.assign("../index.html");
+    alert("Seu cadastro foi concluído com sucesso!")
 })
