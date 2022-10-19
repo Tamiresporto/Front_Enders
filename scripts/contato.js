cf3ba18f5169a17495bbb3ae2d3c4c710cb8484c
Bruna Lima <stephanierannyelle73@gmail.com>
2022-10-19 11:01:39 -0300

    att

--- scripts/contato.js
+++ scripts/contato.js
@@ -21,3 +21,37 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+function width () {
+  
+  const tamanho = getEle
+}
+
+// INICIANDO A ROLAGEM DE CARDS - MOBILE
+
+var swiper = new Swiper(".mySwiperMobile", {
+  slidesPerView: 1,
+  spaceBetween: 1,
+  grabCursor: true,
+  autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+    },
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+function width () {
+
+const tamanho = getEle
+}
